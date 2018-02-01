5b92b0e6bb1eae3d1cd17434f2a48a6e4e8e4aec
Kevin Jacobs <kevin91nl@gmail.com>
2018-02-01 21:42:54 +0100

    - Added a progressbar. - Added a description to the preprocessing script.

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,22 @@
 import argparse
 
+import os
+from tqdm import tqdm
+import time
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description='Add the output of the Stanford CoreNLP pipeline and entity alignment information to the JSON '
+                    'files found in the folder specified by the input argument.')
     parser.add_argument('input',
                         help='Path to the input files (folder containing JSON files).')
-    parser.add_argument('--corenlp_url', default='http://localhost:9000/',
+    parser.add_argument('--corenlp_url',
                         help='URL of the Stanford CoreNLP server.')
+    parser.set_defaults(corenlp_url='http://localhost:9000/')
     args = parser.parse_args()
-    print(args)
+
+    # Test the Stanford CorenLP server
+    progressbar = tqdm(os.listdir(args.input))
+    for file in progressbar:
+        progressbar.set_description(file)
+        time.sleep(2)
