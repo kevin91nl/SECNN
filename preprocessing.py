14333c637b9ef549430c189f6fde2523a9b14ced
Kevin Jacobs <kevin91nl@gmail.com>
2018-02-01 21:24:15 +0100

    - Make input path required.

--- preprocessing.py
+++ preprocessing.py
@@ -2,7 +2,7 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input',
+    parser.add_argument('input',
                         help='Path to the input files (folder containing JSON files).')
     parser.add_argument('--corenlp_url', default='http://localhost:9000/',
                         help='URL of the Stanford CoreNLP server.')
