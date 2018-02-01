5ca96631647220d027b7f6d26af8d1cb578beef5
Kevin Jacobs <kevin91nl@gmail.com>
2018-02-01 21:23:38 +0100

    - Argument for specifying the Stanford CoreNLP server. - Documentation adjusted for the new argument.

--- preprocessing.py
+++ preprocessing.py
@@ -2,4 +2,9 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', help='Path to the input files (folder containing JSON files).')
+    parser.add_argument('--input',
+                        help='Path to the input files (folder containing JSON files).')
+    parser.add_argument('--corenlp_url', default='http://localhost:9000/',
+                        help='URL of the Stanford CoreNLP server.')
+    args = parser.parse_args()
+    print(args)
