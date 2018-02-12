9f6b81e150d55cc9828f5b3d5851802a21d4e212
Kevin Jacobs <kevin91nl@gmail.com>
2018-02-12 17:58:18 +0100

    - Created a function for loading the GloVe weight matrix and words.

--- preprocess/vocab.py
+++ preprocess/vocab.py
@@ -1,5 +1,47 @@
+import csv
+
+import numpy as np
+import pandas as pd
+
 VOCAB_POSTAGS = ['<PAD>', '<UNK>', 'CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'JJ', 'JJR', 'JJS', 'LS', 'MD', 'NN', 'NNS',
                  'NNP', 'NNPS', 'PDT', 'POS', 'PRP', 'PRP$', 'RB', 'RBR', 'RBS', 'RP', 'SYM', 'TO', 'UH', 'VB', 'VBD',
                  'VBG', 'VBN', 'VBP', 'VBZ', 'WDT', 'WP', 'WP$', 'WRB']
 
 VOCAB_ENTITIES = ['<PAD>'] + ['@target'] + ['@entity%d' % i for i in range(1, 128)]
+
+
+def load_glove_file(path):
+    """Load the GloVe file.
+
+    Parameters
+    ----------
+    path : str
+        Path to the GloVe file.
+
+    Returns
+    -------
+    list
+        The word vocabulary (ordered list of words found in the GloVe file).
+    np.matrix
+        A n x m matrix (where n is the number of words in the vocabulary and m is the dimensionality of the vectors)
+        containing the pre-trained weights found in the GloVe file.
+    """
+
+    df_words = pd.read_table(path, sep=' ', index_col=0, header=None, quoting=csv.QUOTE_NONE)
+
+    # Load the vocabulary and the weight matrix
+    vocab = df_words.index.values.tolist()
+    weights = df_words.values
+    words_count, words_dim = weights.shape
+
+    # Initialize vectors for the <PAD> (zeros) and <UNK> (initialized using Xavier initializer) markers
+    xavier_bound = np.sqrt(6.) / np.sqrt(words_count + words_dim)
+    markers_matrix = [np.zeros((1, words_dim))]
+    markers_matrix += [np.random.uniform(-xavier_bound, xavier_bound, (1, words_dim))]
+    markers_matrix = np.vstack(markers_matrix)
+
+    # Augment the word vocabulary with the markers
+    vocab = ['<PAD>', '<UNK>'] + vocab
+    weights = np.vstack([markers_matrix, weights])
+
+    return vocab, weights
--- train.py
+++ train.py
@@ -1,10 +1,7 @@
 import argparse
-import csv
 import os
 
 import chainer
-import numpy as np
-import pandas as pd
 from chainer import training
 from chainer.datasets import TransformDataset, split_dataset
 from chainer.iterators import SerialIterator
@@ -17,6 +14,7 @@ from preprocess.tokens import Tokenizer
 from preprocess.vocab import *
 
 if __name__ == '__main__':
+    # Load the arguments
     parser = argparse.ArgumentParser(
         description='Train the SECNN model on the given preprocessed input files.')
     parser.add_argument('input',
@@ -29,44 +27,28 @@ if __name__ == '__main__':
                         help='Number of test documents used for validation.')
     args = parser.parse_args()
 
-    df_words = pd.read_table(args.glove_file, sep=' ', index_col=0, header=None, quoting=csv.QUOTE_NONE)
-
-    ####################################################################################################################
-
-    # Load the vocabulary and the weight matrix
-    VOCAB_WORDS = df_words.index.values.tolist()
-    W_words = df_words.values
-    words_count, words_dim = W_words.shape
-
-    # Initialize vectors for the <PAD> (zeros) and <UNK> (initialized using Xavier initializer) markers
-    xavier_bound = np.sqrt(6.) / np.sqrt(words_count + words_dim)
-    markers_matrix = [np.zeros((1, words_dim))]
-    markers_matrix += [np.random.uniform(-xavier_bound, xavier_bound, (1, words_dim))]
-    markers_matrix = np.vstack(markers_matrix)
-
-    # Augment the word vocabulary with the markers
-    VOCAB_WORDS = ['<PAD>', '<UNK>'] + VOCAB_WORDS
-    W_words = np.vstack([markers_matrix, W_words])
-
     # Convert vocab lists to dictionaries
+    VOCAB_WORDS, W_words = load_glove_file(args.glove_file)
     VOCAB_WORDS = {word: index for index, word in enumerate(VOCAB_WORDS)}
     VOCAB_POSTAGS = {postag: index for index, postag in enumerate(VOCAB_POSTAGS)}
     VOCAB_ENTITIES = {entity: index for index, entity in enumerate(VOCAB_ENTITIES)}
 
-    ####################################################################################################################
-
+    # Create the tokenizer and the preprocessor
     tokenizer = Tokenizer(vocab_words=VOCAB_WORDS, vocab_postags=VOCAB_POSTAGS, vocab_entities=VOCAB_ENTITIES)
     preprocessor = Preprocessor(tokenizer)
 
     files = [os.path.join(args.input, file) for file in os.listdir(args.input)]
 
+    # Create file loaders and transformations
     file_loader = JSONFileLoader(preprocessor)
     dataset = TransformDataset(files, file_loader.load_file)
-    test_set, train_set = split_dataset(dataset, 1)
 
+    # Split the dataset and initialize the dataset iterators
+    test_set, train_set = split_dataset(dataset, args.test_size)
     train_iter = SerialIterator(train_set, batch_size=1, repeat=True, shuffle=True)
     test_iter = SerialIterator(test_set[:args.test_size], batch_size=args.test_size, repeat=False, shuffle=False)
 
+    # Initialize the model
     model = SECNN(
         config_word={'in_size': W_words.shape[0], 'out_size': W_words.shape[1], 'initialW': W_words},
         config_postag={'in_size': len(VOCAB_POSTAGS), 'out_size': 32},
@@ -77,9 +59,11 @@ if __name__ == '__main__':
     loss_model = SECNNLossWrapper(model)
     model.embed_word.disable_update()
 
+    # Setup the optimizer
     optimizer = chainer.optimizers.SGD()
     optimizer.setup(model)
 
+    # Create the updater and trainer
     updater = training.StandardUpdater(train_iter, optimizer=optimizer, converter=lambda *arguments: arguments[0],
                                        loss_func=loss_model.__call__, device=-1)
     trainer = training.Trainer(updater, (1, 'epoch'), out='result')
@@ -90,7 +74,9 @@ if __name__ == '__main__':
     trainer.extend(extensions.ProgressBar())
     trainer.extend(extensions.snapshot(), trigger=(100, 'iteration'))
 
+    # Resume from a specified snapshot
     if args.resume:
         chainer.serializers.load_npz(args.resume, trainer)
 
+    # Run the trainer
     trainer.run()
